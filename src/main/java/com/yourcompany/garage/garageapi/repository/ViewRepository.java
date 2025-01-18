53d83354f9a31138283f9033e9a13510f6ed9d8b
Michaprog <vejnstejn2@gmail.com>
2025-01-18 15:37:04 +0100

    none

--- src/main/java/com/yourcompany/garage/garageapi/repository/ViewRepository.java
+++ src/main/java/com/yourcompany/garage/garageapi/repository/ViewRepository.java
@@ -8,9 +8,21 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ViewRepository extends JpaRepository<VoitureEnVenteDTO, Integer> {
+public interface ViewRepository{
 
-    @Query(value = "SELECT * FROM vue_voitures_en_vente", nativeQuery = true)
+
+    @Query(value = "SELECT numerochassis AS numeroChassis, " +
+            "marque, " +
+            "typecarrosserie AS typeCarrosserie, " +
+            "couleur, " +
+            "prix, " +
+            "nombreportes AS nombrePortes, " +
+            "puissance, " +
+            "consommation, " +
+            "neuf, " +
+            "garantie, " +
+            "datefingarantie AS dateFinGarantie " +
+            "FROM vue_voitures_en_vente", nativeQuery = true)
     List<VoitureEnVenteDTO> findAllCarsInSale();
 
 }
